6921dda22afcba4a9ffee7523779c8b179c4f497
Michal Charmas <michal@charmas.pl>
2011-10-01 12:03:49 +0200

    project tree

---
+++ src/pl/charmas/GistNotesActivity.java
@@ -0,0 +1,13 @@
+package pl.charmas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GistNotesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
